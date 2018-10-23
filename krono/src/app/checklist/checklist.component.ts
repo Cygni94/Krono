53d1f9310a963e62b077874ee8310685668404ab
Cristiano Coelho <ccoelho.me@gmail.com>
2018-10-22 21:59:40 -0300

    oks

--- krono/src/app/checklist/checklist.component.ts
+++ krono/src/app/checklist/checklist.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-checklist',
-  templateUrl: './checklist.component.html',
-  styleUrls: ['./checklist.component.scss']
+  selector: "app-checklist",
+  templateUrl: "./checklist.component.html",
+  styleUrls: ["./checklist.component.scss"]
 })
 export class ChecklistComponent implements OnInit {
+  label: "Tarefa 1";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
