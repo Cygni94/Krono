a1a94d0307568f3cb8019efc1b4584ab723349f2
coelhojs <ccoelho.me@gmail.com>
2019-07-31 21:16:20 -0300

    estrutura desejada

--- server/model/task.js
+++
@@ -1,9 +0,0 @@
-class Task {
-    constructor(name, dueDate) {
-        this.id = 0;
-        this.name = name;
-        this.creationDate = new Date();
-        this.dueDate = dueDate;
-    }
-}
-//# sourceMappingURL=task.js.map
