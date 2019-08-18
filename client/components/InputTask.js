5008b44248086a353656952796f80cd2c2fe2b83
coelhojs <ccoelho.me@gmail.com>
2019-08-18 17:49:42 -0300

    preparando ambiente para debugging

--- client/components/InputTask.js
+++ client/components/InputTask.js
@@ -1,22 +1,23 @@
 import DateFnsUtils from '@date-io/date-fns';
-import {
-    MuiPickersUtilsProvider,
-    KeyboardTimePicker,
-    KeyboardDatePicker,
-} from '@material-ui/pickers';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import IconButton from '@material-ui/core/IconButton';
 import InputBase from '@material-ui/core/InputBase';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import Switch from '@material-ui/core/Switch';
 import Typography from '@material-ui/core/Typography';
 import AddIcon from '@material-ui/icons/Add';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import clsx from 'clsx';
 import React from 'react';
 
@@ -44,7 +45,14 @@ const useStyles = makeStyles(theme => ({
     },
     panel: {
         width: '100%'
-    }
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
 }));
 
 export default function InputTask() {
@@ -59,6 +67,27 @@ export default function InputTask() {
     const handleChange = name => event => {
         setValues({ ...values, [name]: event.target.value });
     };
+
+    // function handleChange(event) {
+    //     setValues(oldValues => ({
+    //         ...oldValues,
+    //         [event.target.name]: event.target.value,
+    //     }));
+    // }
+
+    const [state, setState] = React.useState({
+        age: '',
+        name: 'hai',
+    });
+
+    const inputLabel = React.useRef(null);
+
+    // const handleChange = name => event => {
+    //     setState({
+    //         ...state,
+    //         [name]: event.target.value,
+    //     });
+    // };
 
     function handleDateChange(date) {
         setSelectedDate(date);
@@ -111,6 +140,31 @@ export default function InputTask() {
                                     variant="outlined"
                                 />
                             </MuiPickersUtilsProvider >
+                            <FormControlLabel
+                                value="repetir"
+                                control={<Switch color="primary" />}
+                                label="Repetir"
+                                labelPlacement="start"
+                            />
+                            <FormControl className={classes.formControl}>
+                                <InputLabel htmlFor="age-native-simple">Recorrência</InputLabel>
+                                <Select
+                                    value={state.age}
+                                    onChange={handleChange('age')}
+                                    inputProps={{
+                                        name: 'age',
+                                        id: 'age-native-simple',
+                                    }}
+                                >
+                                    <MenuItem value="" />
+                                    <MenuItem value="diariamente">Diariamente</MenuItem>
+                                    <MenuItem value="dias-uteis">Dias úteis</MenuItem>
+                                    <MenuItem value="semanalmente">Toda semana</MenuItem>
+                                    <MenuItem value="mensalmente">Todo mês</MenuItem>
+                                    <MenuItem value="anualmente">Anualmente</MenuItem>
+                                    <MenuItem value="personalizado">Personalizado</MenuItem>
+                                </Select>
+                            </FormControl>
                         </form>
                     </div>
                 </ExpansionPanelDetails>
